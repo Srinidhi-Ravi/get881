e34d984984f8199d3eb7da5a69e9ac495067171c
Srinidhi-Ravi <76148899+Srinidhi-Ravi@users.noreply.github.com>
2020-12-18 15:40:40 +0530

    Update Calc.java

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -1,10 +1,10 @@
 package com.lti.model;
 
 public class Calc {
-	public void add(int n1, int n2) {
+	public void sum(int n1, int n2) {
 		System.out.println("Sum is "+(n1+n2));
 	}
-	public void diff(int n1, int n2) {
+	public void sub(int n1, int n2) {
 		System.out.println("Difference is "+(n1-n2));
 	}
 	public void mul(int n1, int n2) {
